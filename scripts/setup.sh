e23c24f9ef20a1865ed5bbac61aa26d8b05c1923
Yang Yang <yyang.even@gmail.com>
2021-02-01 16:31:23 -0800

    Common: Setup the project structure
    Change-Id: I2ece2ebc73be2bea14e6a6e9c2733be46f8a7f28

---
+++ scripts/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# This script sets up a project to be used for the first time
+#
+
+set -ex
+
+source ~/.bash_util.sh
+
+THIS_DIR=$(dirname "$0")
+
+QuietRun pushd "$THIS_DIR"
+PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
+QuietRun popd
+
+QuietRun pushd "$PROJECT_ROOT_DIR"
+PROJECT_NAME=$(basename "$PROJECT_ROOT_DIR")
+echo "Setting up project \"$PROJECT_NAME\"."
+
+./scripts/bootstrap.sh
+
+mkdir build
+pushd build
+cmake .. -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo
+popd
+make -j2
+
+../yyLinuxConfig/scripts/setup_ctags_for.sh
+QuietRun popd
