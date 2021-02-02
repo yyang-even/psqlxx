28c368aecfea050fb0d381292704d4d5102b998e
Yang Yang <yyang.even@gmail.com>
2021-02-01 16:31:23 -0800

    Common: Setup the project structure
    Change-Id: I2ece2ebc73be2bea14e6a6e9c2733be46f8a7f28

---
+++ cmake/utils.cmake
@@ -0,0 +1,27 @@
+# add_gtest_for
+function (add_gtest_for source_name)
+    if (BUILD_TESTING)
+        include(GoogleTest)
+
+        set(target_name "${source_name}.test")
+        add_executable(${target_name} ${target_name}.cpp ${source_name}.cpp)
+        target_link_libraries(${target_name} PRIVATE gtest_main ${ARGN})
+        gtest_discover_tests(${target_name} PROPERTIES LABELS ${PROJECT_NAME})
+
+        if (pclient_WANT_AUTO_TESTS)
+            add_custom_command(
+                TARGET ${target_name}
+                POST_BUILD
+                COMMAND ctest --output-on-failure -R ^${target_name}$
+                COMMENT "Testing '${target_name}'"
+                VERBATIM)
+        endif ()
+    endif ()
+endfunction ()
+
+# config_compiler_and_linker
+macro (CONFIG_CXX_COMPILER_AND_LINKER)
+    set(CMAKE_CXX_STANDARD 17)
+    set(CMAKE_CXX_STANDARD_REQUIRED ON)
+    set(CMAKE_CXX_EXTENSIONS OFF)
+endmacro ()
