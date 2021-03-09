6249cc6f17d0368bb6bc94243171e7c3cb09551e
Yang Yang <yyang.even@gmail.com>
2021-03-09 12:13:19 -0800

    3rdParty: Add libpqxx as dependency
    Change-Id: Icaf3bb44d3d7f9b619a9794a37fb42155ceba6d5

--- postgres_client/main.cpp
+++ postgres_client/main.cpp
@@ -1,13 +1,32 @@
 #include <postgres_client/args.hpp>
 
+#include <pqxx/pqxx>
+
 
 using namespace postgres_client;
 
+
+void DoQuery() {
+    try {
+        pqxx::connection c;
+        pqxx::work w(c);
+
+        pqxx::row r = w.exec1("SELECT 1");
+
+        w.commit();
+
+        std::cout << "Result: " << r[0].as<int>() << std::endl;
+    } catch (std::exception const &e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
 
 int main(int argc, char **argv) {
     auto options = BuildOptions();
 
     HandleOptions(options, argc, argv);
+
+    DoQuery();
 
     return EXIT_SUCCESS;
 }
