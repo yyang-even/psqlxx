6a9502bc805821fd45318ccfea90ef4c3b9844f4
Yang Yang <yyang.even@gmail.com>
2021-03-24 15:46:47 -0700

    Tests: Add test_db_cleanup and test_db_setup
    Change-Id: I41d243901c7fca6b5edc0243fa3fe942d864d452

---
+++ tests/db/test_db_cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# This script clean up DB for tests
+#
+
+set -ex
+
+THIS_DIR=$(dirname "$0")
+
+source $THIS_DIR/test_db_defines.sh
+
+if !psql --command "SELECT 1;" &> /dev/null; then
+    CMD_PREFIX="sudo -u postgres"
+fi
+
+$CMD_PREFIX dropdb --if-exists $SHARED_DB_NAME
+
+$CMD_PREFIX dropuser --if-exists $READ_USER_NAME
+
+$CMD_PREFIX dropuser --if-exists $ADMIN_USER_NAME
---
+++ tests/db/test_db_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# This script setup DB for tests
+#
+
+set -ex
+
+THIS_DIR=$(dirname "$0")
+
+source $THIS_DIR/test_db_defines.sh
+
+if !psql --command "SELECT 1;" &> /dev/null; then
+    CMD_PREFIX="sudo -u postgres"
+fi
+
+$CMD_PREFIX psql --command "CREATE USER $ADMIN_USER_NAME WITH CREATEDB NOCREATEROLE LOGIN PASSWORD '$ADMIN_USER_PASSWORD';"
+
+$CMD_PREFIX psql --command "CREATE USER $READ_USER_NAME WITH NOCREATEDB NOCREATEROLE LOGIN PASSWORD '$READ_USER_PASSWORD';"
+
+$CMD_PREFIX createdb --owner=$ADMIN_USER_NAME $SHARED_DB_NAME
