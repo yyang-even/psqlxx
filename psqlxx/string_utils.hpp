9608076b95a78bea0a57ddca63dfa5c59e9dbdb9
Yang Yang <yyang.even@gmail.com>
2021-03-25 16:34:00 -0700

    Tests: Add real_db.test
    Change-Id: I88a2a723b5eebcede345a55285848cef52e5cfb4

--- psqlxx/string_utils.hpp
+++ psqlxx/string_utils.hpp
@@ -5,9 +5,13 @@
 #include <string>
 
 
+namespace psqlxx {
+
 [[nodiscard]]
 static inline bool StartsWith(const std::string &str, const char *prefix) {
     assert(prefix);
 
     return str.rfind(prefix, 0) == 0;
 }
+
+}//namespace psqlxx
