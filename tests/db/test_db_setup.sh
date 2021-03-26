40c1b8ba7f895791265c97f0f19c59f18c0d037b
Yang Yang <yyang.even@gmail.com>
2021-03-26 13:00:58 -0700

    CI: Fix CI
    Change-Id: Ib5ef524b9c04c5651057082f1a9ad6240ef56911

--- tests/db/test_db_setup.sh
+++ tests/db/test_db_setup.sh
@@ -8,7 +8,9 @@ set -ex
 
 source test_db_defines.sh
 
-if psql --command "SELECT 1;" &> /dev/null; then
+SQL_VERSION="SELECT version();"
+
+if psql --command "$SQL_VERSION" &> /dev/null; then
     CMD_PREFIX=""
 else
     CMD_PREFIX="sudo -u postgres"
@@ -20,5 +22,5 @@ $CMD_PREFIX psql --command "CREATE USER $READ_USER_NAME WITH NOCREATEDB NOCREATE
 
 $CMD_PREFIX createdb --owner=$ADMIN_USER_NAME $SHARED_DB_NAME
 
-psql --host=$DB_HOST --command "SELECT 1;" "dbname=$SHARED_DB_NAME user=$ADMIN_USER_NAME password='$ADMIN_USER_PASSWORD'"
-psql --host=$DB_HOST --command "SELECT 1;" "dbname=$SHARED_DB_NAME user=$READ_USER_NAME password='$READ_USER_PASSWORD'"
+psql --host=$DB_HOST --command "$SQL_VERSION" "dbname=$SHARED_DB_NAME user=$ADMIN_USER_NAME password='$ADMIN_USER_PASSWORD'"
+psql --host=$DB_HOST --command "$SQL_VERSION" "dbname=$SHARED_DB_NAME user=$READ_USER_NAME password='$READ_USER_PASSWORD'"
