6bb0aca3333d9fcf6d3c2a45028cddbcdaa6f931
Yang Yang <yyang.pplus@gmail.com>
2021-03-26 12:12:27 -0700

    Test: Fix real_db.test on CI
    Change-Id: Id293c37bd413b7445ef7eacf6bb6a78eeea3df6e

--- psqlxx/test_utils.hpp
+++ psqlxx/test_utils.hpp
@@ -14,7 +14,7 @@ const std::string GetDbHostParameter();
 const std::string GetSharedDbNameParameter();
 
 [[nodiscard]]
-const std::string GetViewerConnectionString();
+const std::string GetViewerConnectionString(const bool with_password = true);
 
 [[nodiscard]]
 const std::string GetAdminConnectionString();
